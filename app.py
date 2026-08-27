d1abcbc0eb76f9e522ec56434652724bdc5a1086
Rik Dutta <officialrikdutta@gmail.com>
2026-08-28 01:40:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,8 @@ USERS = {
     "riya": "riya123", "aman": "aman123", "priya": "priya456",
     "rohit": "rohit789", "sneha": "sneha123", "vikas": "vikas123",
     "ananya": "ananya123", "karan": "karan123", "pooja": "pooja123", "rahul": "rahul123" ,
-    "rik": "rik789", "anshu": "anshu123", "yoshita": "yoshi123","shouvik": "shouvik123", "ridhibrata": "ridhi123" ,"soumoda":"dada420"
+    "rik": "rik789", "anshu": "anshu123", "yoshita": "yoshi123","shouvik": "shouvik123", "ridhibrata": "ridhi123" ,"soumoda":"dada420",
+    "manishda": "dadabolekotha", "shreyanks": "shrey696969"
     
 }
 ADMIN_USERNAME = 'admin'
